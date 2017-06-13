069d4ca6504a190d3d7e8b84ef55041e636f0a06
Thomas Diesler <thomas.diesler@jboss.com>
2017-06-13 12:02:36 +0200

    Move to java based caps/reqs

--- src/main/java/org/fuse/cidpoc/d/D.java
+++ src/main/java/org/fuse/cidpoc/d/D.java
@@ -3,15 +3,26 @@ package org.fuse.cidpoc.d;
 import java.util.Arrays;
 import java.util.List;
 
-import org.fuse.cidpoc.a.Item;
+import org.fuse.cidpoc.Item;
+import org.fuse.cidpoc.a.A;
 import org.fuse.cidpoc.b.B;
 
-public class D implements Item {
-
-    Item[] deps = new Item[] { new B() };
+public class D extends Item {
 
     @Override
-    public List<Item> getDependecies() {
+    public Capability getCapability() {
+        return new Capability("D", 5);
+    }
+
+    @Override
+    public List<Requirement> getRequirements() {
+        Requirement[] reqs = new Requirement[] { new Requirement("A", 3, 6), new Requirement("B", 3, 6) };
+        return Arrays.asList(reqs);
+    }
+
+    @Override
+    public List<Item> getDependencies() {
+        Item[] deps = new Item[] { new A(), new B() };
         return Arrays.asList(deps);
     }
 }
