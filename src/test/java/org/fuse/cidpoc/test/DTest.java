e297103610bb88f0a53837683b37cb23e6636e95
Thomas Diesler <thomas.diesler@jboss.com>
2017-06-13 12:02:36 +0200

    Move to java based caps/reqs

--- src/test/java/org/fuse/cidpoc/test/DTest.java
+++ src/test/java/org/fuse/cidpoc/test/DTest.java
@@ -1,7 +1,12 @@
 package org.fuse.cidpoc.test;
 
-import org.fuse.cidpoc.a.Item;
-import org.fuse.cidpoc.a.Utils;
+import java.util.List;
+
+import org.fuse.cidpoc.Item;
+import org.fuse.cidpoc.Item.Capability;
+import org.fuse.cidpoc.Item.Requirement;
+import org.fuse.cidpoc.a.A;
+import org.fuse.cidpoc.b.B;
 import org.fuse.cidpoc.d.D;
 import org.junit.Assert;
 import org.junit.Test;
@@ -14,8 +19,26 @@ public class DTest {
         Item item = new D();
         item.transitiveStatus();
         
-        String version = Utils.getVersion(item.getClass());
-        String status = Utils.getStatus(item.getClass());
-        Assert.assertEquals("D-" + version + " is " + status, item.getMessage());
+        List<Item> deps = item.getDependencies();
+        Assert.assertEquals(2, deps.size());
+        Item depA = deps.get(0);
+        Item depB = deps.get(1);
+        Assert.assertEquals("A-" + Item.getVersion(A.class), depA.getVName());
+        Assert.assertEquals("B-" + Item.getVersion(B.class), depB.getVName());
+        
+        Capability cap = item.getCapability();
+        Assert.assertEquals("D", cap.getName());
+        Assert.assertTrue(cap.getValue() > 0);
+        
+        List<Requirement> reqs = item.getRequirements();
+        Assert.assertEquals(2, reqs.size());
+        Requirement reqA = reqs.get(0);
+        Requirement reqB = reqs.get(1);
+        Assert.assertTrue(reqA.matches(depA.getCapability()));
+        Assert.assertTrue(reqB.matches(depB.getCapability()));
+
+        String version = Item.getVersion(item.getClass());
+        Assert.assertEquals("D-" + version, item.getVName());
+        Assert.assertEquals("is satisfied", item.getStatus());
     }
 }
